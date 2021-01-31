c8ea687859d0b4333573d2b757f8f73c0e3632d2
dkstjdwns114 <sungjoon256@gmail.com>
2021-01-31 19:25:22 +0900

    회원가입하는중

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,86 @@
 import React, { Component } from "react";
 
+import "./Auth.css";
+
 class AuthPage extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+    this.nicknameEl = React.createRef();
+    this.state = {
+      isExist: false
+    };
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+    const username = this.nicknameEl.current.value.toLowerCase();
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    const requestBody = {
+      query: `
+        mutation {
+          createUser(userInput: {email: "${email}", password: "${password}", username: "${username}"}) {
+            _id
+            email
+          }
+        }
+      `
+    };
+
+    fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        if (resData.data.createUser === null) {
+          console.log("if문에 들어왔다");
+          this.setState({ isExist: true });
+        }
+        console.log(resData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
-    return <h1>The Auth Page</h1>;
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="nickname">nickname</label>
+          <input type="text" id="nickname" ref={this.nicknameEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button">Switch to Signup</button>
+        </div>
+        {this.state.isExist ? <p>이미 존재하는 이메일/비밀번호 입니다.</p> : ""}
+      </form>
+    );
   }
 }
 
