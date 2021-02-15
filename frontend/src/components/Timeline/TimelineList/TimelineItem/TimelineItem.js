14cd9975f60d5c88096e12913b1a9088802273de
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-15 23:55:17 +0900

    게시물 상세보기 버튼 수정

--- frontend/src/components/Timeline/TimelineList/TimelineItem/TimelineItem.js
+++ frontend/src/components/Timeline/TimelineList/TimelineItem/TimelineItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const timelineItem = (props) => (
   <li key={props.mediaId} className="events__list-item">
@@ -9,23 +10,10 @@ const timelineItem = (props) => (
     <p>
       좋아요 {props.mediaLiked.length}명 | 댓글 {props.comment.length}개
     </p>
-    {(props.mediaLiked.length !== 0 || props.comment.length !== 0) && <hr />}
-    {props.mediaLiked.length !== 0 && (
-      <button
-        className="btn"
-        onClick={props.likeDetail.bind(this, props.mediaId)}
-      >
-        좋아요 상세보기
-      </button>
-    )}
-    {props.comment.length !== 0 && (
-      <button
-        className="btn"
-        onClick={props.commentDetail.bind(this, props.mediaId)}
-      >
-        댓글 {props.comment.length}개 상세보기
-      </button>
-    )}
+    <hr />
+    <Link to={"/p/" + props.mediaId} className="btn post-detail">
+      게시물 상세보기
+    </Link>
     <div>
       {/* props.userId === props.creatorId  // 이 게시물이 내가 쓴것인지 확인하는 코드*/}
     </div>
