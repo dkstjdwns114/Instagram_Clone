82d18ee4ffebdc703634fd8afc23dcd52767a041
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-17 00:21:15 +0900

    타임라인 최근 글이 위로 올라오게 Reverse

--- frontend/src/components/Timeline/TimelineList/TimelineList.js
+++ frontend/src/components/Timeline/TimelineList/TimelineList.js
@@ -3,28 +3,16 @@ import React from "react";
 import TimelineItem from "./TimelineItem/TimelineItem";
 
 const timelineList = (props) => {
-  const medias = props.medias.map((media) => {
-    const comment = media.commentTexts.map((media_caption, idx) => {
-      return (
-        <p key={media_caption._id}>
-          <span key={media_caption.creator._id}>
-            {media_caption.creator.username}
-          </span>{" "}
-          : {media_caption.media_comment}
-        </p>
-      );
-    });
+  const medias = props.medias.reverse().map((media) => {
     return (
       <TimelineItem
         mediaId={media._id}
         mediaUrl={media.media_url}
         mediaCaption={media.media_caption}
         mediaLiked={media.likeds}
-        comment={comment}
+        commentCnt={media.commentTexts.length}
         creatorId={media.creator._id}
         userId={props.authUserId}
-        likeDetail={props.onLikeDetail}
-        commentDetail={props.onCommentDetail}
       />
     );
   });
