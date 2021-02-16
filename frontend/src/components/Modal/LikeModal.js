b2492706b0044d7ee779a2531b03510475f2e621
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-17 00:17:14 +0900

    게시물 상세페이지 Like Modal 완료

--- frontend/src/components/Modal/LikeModal.js
+++ frontend/src/components/Modal/LikeModal.js
@@ -36,25 +36,27 @@ const likeModal = (props) => (
         </div>
         <div className="modal-body">
           <p style={modalBodyTextStyle}>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
-            <p>이미지, 닉네임</p>
+            {props.likes.map((like, idx) => {
+              return (
+                <div className="social-header" key={like.user.username}>
+                  <a href="/gllcollege/" className="social-profile-img">
+                    <img
+                      src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/131994802_214284176856542_767716126641019031_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=PDorBUdEWtwAX9sYsr4&tp=1&oh=cf29f6436cf30c756daf0b3772249b4e&oe=60534882"
+                      alt="gllcollege's profile picture"
+                    />
+                  </a>
+                  <div className="social-follow">
+                    <a
+                      title="gllcollege"
+                      href="/gllcollege/"
+                      className="social-name"
+                    >
+                      {like.user.username}
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
           </p>
         </div>
         <div className="modal-footer">
