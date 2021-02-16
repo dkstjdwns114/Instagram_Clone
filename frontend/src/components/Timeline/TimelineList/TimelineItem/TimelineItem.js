e85b43def3f36efe4b4a048c67f69337cf2444cd
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-17 00:20:38 +0900

    타임라인 게시물에 Creator 프로필, 아이디 추가

--- frontend/src/components/Timeline/TimelineList/TimelineItem/TimelineItem.js
+++ frontend/src/components/Timeline/TimelineList/TimelineItem/TimelineItem.js
@@ -3,12 +3,28 @@ import { Link } from "react-router-dom";
 
 const timelineItem = (props) => (
   <li key={props.mediaId} className="events__list-item">
+    <div className="social-header">
+      <a href="/gllcollege/" className="social-profile-img">
+        <img
+          src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/131994802_214284176856542_767716126641019031_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=PDorBUdEWtwAX9sYsr4&tp=1&oh=cf29f6436cf30c756daf0b3772249b4e&oe=60534882"
+          alt="gllcollege's profile picture"
+        />
+      </a>
+      <div className="social-follow">
+        <a title="gllcollege" href="/gllcollege/" className="social-name">
+          테스트아이디
+        </a>
+        <span className="SPAN_13">•</span>
+        <button type="button">Follow</button>
+      </div>
+    </div>
+    <hr />
     <img key={props.mediaId} src={props.mediaUrl} alt={props.mediaUrl} />
     <br />
     {props.mediaCaption}
     <hr />
     <p>
-      좋아요 {props.mediaLiked.length}명 | 댓글 {props.comment.length}개
+      좋아요 {props.mediaLiked.length}명 | 댓글 {props.commentCnt}개
     </p>
     <hr />
     <Link to={"/p/" + props.mediaId} className="btn post-detail">
