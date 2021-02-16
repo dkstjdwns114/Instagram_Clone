136f81d1786e2b6da1c0fe50a83dccd55378cef6
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-16 17:46:49 +0900

    Test 페이지 수정

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -5,43 +5,43 @@ import "./PostDetail.css";
 class TestPage extends Component {
   render() {
     return (
-      <article class="social-article">
-        <div class="social-left-col">
-          <div class="social-img-wrap">
+      <article className="social-article">
+        <div className="social-left-col">
+          <div className="social-img-wrap">
             <img
-              class="social-img"
+              className="social-img"
               src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/149685140_1084278472088595_2002598999607769655_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=m48d3YfhIp0AX8egkUC&tp=1&oh=34dc2d8ed6a17e6dfbb9a5caded5f400&oe=605446E2"
             />
           </div>
         </div>
-        <div class="social-right-col">
-          <div class="social-header">
-            <a href="/gllcollege/" class="social-profile-img">
+        <div className="social-right-col">
+          <div className="social-header">
+            <a href="/gllcollege/" className="social-profile-img">
               <img
                 src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/131994802_214284176856542_767716126641019031_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=PDorBUdEWtwAX9sYsr4&tp=1&oh=cf29f6436cf30c756daf0b3772249b4e&oe=60534882"
                 alt="gllcollege's profile picture"
               />
             </a>
-            <div class="social-follow">
-              <a title="gllcollege" href="/gllcollege/" class="social-name">
+            <div className="social-follow">
+              <a title="gllcollege" href="/gllcollege/" className="social-name">
                 지수입니다
               </a>
-              <span class="SPAN_13">•</span>
+              <span className="SPAN_13">•</span>
               <button type="button">Follow</button>
             </div>
           </div>
-          <div class="social-comments-wrap">
-            <div class="social-post">
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+          <div className="social-comments-wrap">
+            <div className="social-post">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -50,16 +50,16 @@ class TestPage extends Component {
                   <time>1w</time>
                 </div>
               </div>
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -68,16 +68,16 @@ class TestPage extends Component {
                   <time>1w</time>
                 </div>
               </div>
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -86,16 +86,16 @@ class TestPage extends Component {
                   <time>1w</time>
                 </div>
               </div>
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -104,16 +104,16 @@ class TestPage extends Component {
                   <time>1w</time>
                 </div>
               </div>
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -122,16 +122,16 @@ class TestPage extends Component {
                   <time>1w</time>
                 </div>
               </div>
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -140,16 +140,16 @@ class TestPage extends Component {
                   <time>1w</time>
                 </div>
               </div>
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -158,16 +158,16 @@ class TestPage extends Component {
                   <time>1w</time>
                 </div>
               </div>
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -176,16 +176,16 @@ class TestPage extends Component {
                   <time>1w</time>
                 </div>
               </div>
-              <div class="social-header">
-                <a href="/gllcollege/" class="social-profile-img">
+              <div className="social-header">
+                <a href="/gllcollege/" className="social-profile-img">
                   <img
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/93220711_3143709632340205_7652637101535526912_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jw8owoE_3kEAX8tlHZD&tp=1&oh=d398d00901eda38513c1bf88ab571022&oe=6052768E"
                     alt="gllcollege's profile picture"
                   />
                 </a>
-                <div class="social-copy">
-                  <span class="social-name">댓글닉네임</span>
-                  <span class="social-post-copy">
+                <div className="social-copy">
+                  <span className="social-name">댓글닉네임</span>
+                  <span className="social-post-copy">
                     {" "}
                     Swimming into the long weekend like Swimming into the long
                     weekend like Swimming into the long weekend like Swimming
@@ -195,36 +195,36 @@ class TestPage extends Component {
                 </div>
               </div>
             </div>
-            <div class="social-icons">
-              <section class="icons-section">
-                <button class="icons-button">
-                  <span class="icon1"></span>
+            <div className="social-icons">
+              <section className="icons-section">
+                <button className="icons-button">
+                  <span className="icon1"></span>
                 </button>
-                <button class="icons-button">
-                  <span class="icon2"></span>
+                <button className="icons-button">
+                  <span className="icon2"></span>
                 </button>
-                <button class="icons-button">
-                  <span class="icon3"></span>
+                <button className="icons-button">
+                  <span className="icon3"></span>
                 </button>
-                <button class="icons-button">
-                  <span class="icon4"></span>
+                <button className="icons-button">
+                  <span className="icon4"></span>
                 </button>
               </section>
-              <div class="likes-wrap">
+              <div className="likes-wrap">
                 <span>16</span> likes
               </div>
             </div>
-            <div class="social-date">
+            <div className="social-date">
               <time>April 18</time>
             </div>
-            <div class="comment-add">
+            <div className="comment-add">
               <form>
                 <input
                   type="text"
                   id="commentarea"
                   placeholder="Add a comment..."
                 />
-                <input type="button" class="btn" value="SUBMIT" />
+                <input type="button" className="btn" value="SUBMIT" />
               </form>
             </div>
           </div>
