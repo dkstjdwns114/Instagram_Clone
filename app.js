24cf27bb3c960c981f96981b5670ffc2a973831b
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-19 01:30:25 +0900

    서버에서 Cloudinary 처리하기

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
+const { cloudinary } = require("./utils/cloudinary");
+
+const app = express();
 
 const graphQlSchema = require("./graphql/schema/index");
 const graphQlResolvers = require("./graphql/resolvers/index");
 
 const isAuth = require("./middleware/is-auth");
 
-const app = express();
+let secure_url;
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.get("/api/image", async (req, res) => {
+  res.send({ url: secure_url });
+});
 
 app.use(bodyParser.json());
 
@@ -33,12 +43,32 @@ app.use(
   })
 );
 
+app.post("/api/upload", async (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "anstagram"
+    });
+    console.log(uploadedResponse);
+
+    secure_url = uploadedResponse.secure_url;
+
+    res.json({ msg: "Successed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.6v20o.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(8000);
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
   })
   .catch((err) => {
     console.log(err);
