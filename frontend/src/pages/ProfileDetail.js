070642a5a68209550e635fb13c67dce2fb379727
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-20 19:34:40 +0900

    Profile Detail Page 기초 완료

--- frontend/src/pages/ProfileDetail.js
+++ frontend/src/pages/ProfileDetail.js
@@ -8,7 +8,10 @@ import AuthContext from "../context/auth-context";
 
 class ProfileDetail extends Component {
   state = {
-    isLoading: false
+    isLoading: false,
+    profile_pic_url: "",
+    createdMedias: [],
+    username: ""
   };
 
   static contextType = AuthContext;
@@ -24,6 +27,7 @@ class ProfileDetail extends Component {
         query{
           userData(username: "${this.props.match.params.username}"){
             _id
+            username
             profile_pic_url
             createdMedias {
               _id
@@ -49,7 +53,12 @@ class ProfileDetail extends Component {
       })
       .then((resData) => {
         console.log(resData);
-        this.setState({ isLoading: false });
+        this.setState({
+          isLoading: false,
+          profile_pic_url: resData.data.userData.profile_pic_url,
+          createdMedias: resData.data.userData.createdMedias.reverse(),
+          username: resData.data.userData.username
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -69,13 +78,13 @@ class ProfileDetail extends Component {
               <div className="profileDetail_profile">
                 <div className="profile_img">
                   <img
-                    src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/131994802_214284176856542_767716126641019031_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Oy1gj4z4hu0AX9uvUp1&tp=1&oh=b30589d5f4b2d57670f1593a8d915a9d&oe=60573D02"
-                    alt=""
+                    src={this.state.profile_pic_url}
+                    alt={this.state.profile_pic_url}
                   />
                 </div>
                 <div className="info">
                   <div className="area_text">
-                    <h2 className="user_id">duthegee</h2>
+                    <h2 className="user_id">{this.state.username}</h2>
                     <Link to="" className="profile_edit">
                       프로필 편집
                     </Link>
@@ -86,20 +95,22 @@ class ProfileDetail extends Component {
                   <div className="area_text">
                     <div className="tit_desc">
                       <span className="title">게시물</span>
-                      <span className="sub_title">7</span>
+                      <span className="sub_title">
+                        {this.state.createdMedias.length}
+                      </span>
                     </div>
                     <div className="tit_desc">
                       <span className="title">팔로워</span>
-                      <span className="sub_title">10</span>
+                      <span className="sub_title">XX</span>
                     </div>
                     <div className="tit_desc">
                       <span className="title">팔로우</span>
-                      <span className="sub_title">15</span>
+                      <span className="sub_title">XX</span>
                     </div>
                   </div>
                   <div className="area_text profile_info">
-                    <h3 className="info_title">안녕하세요.</h3>
-                    <p className="info_sub">네 지나가세요~</p>
+                    <h3 className="info_title">User full name</h3>
+                    <p className="info_sub">한줄소개</p>
                   </div>
                 </div>
               </div>
@@ -137,86 +148,21 @@ class ProfileDetail extends Component {
                 </div>
                 <div className="boards">
                   <ul className="board_list">
-                    <li>
-                      <Link to="#">
-                        <div className="board_img">
-                          <img
-                            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/131994802_214284176856542_767716126641019031_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Oy1gj4z4hu0AX9uvUp1&tp=1&oh=b30589d5f4b2d57670f1593a8d915a9d&oe=60573D02"
-                            alt="프로필 이미지"
-                          />
-                        </div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="#">
-                        <div className="board_img">
-                          <img
-                            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/131994802_214284176856542_767716126641019031_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Oy1gj4z4hu0AX9uvUp1&tp=1&oh=b30589d5f4b2d57670f1593a8d915a9d&oe=60573D02"
-                            alt="메인 프로필"
-                          />
-                        </div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="#">
-                        <div className="board_img">
-                          <img
-                            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/151213134_134563205204831_6028190961915282611_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=04BXqyVT0bMAX9RJshx&tp=1&oh=d91e13721a3ace2311d5fbe4841228b2&oe=60576B66"
-                            alt="리버풀"
-                          />
-                        </div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="#">
-                        <div className="board_img">
-                          <img
-                            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/151213134_134563205204831_6028190961915282611_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=04BXqyVT0bMAX9RJshx&tp=1&oh=d91e13721a3ace2311d5fbe4841228b2&oe=60576B66"
-                            alt="리버풀"
-                          />
-                        </div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="#">
-                        <div className="board_img">
-                          <img
-                            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/151213134_134563205204831_6028190961915282611_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=04BXqyVT0bMAX9RJshx&tp=1&oh=d91e13721a3ace2311d5fbe4841228b2&oe=60576B66"
-                            alt="리그오브레전드"
-                          />
-                        </div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="#">
-                        <div className="board_img">
-                          <img
-                            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/151213134_134563205204831_6028190961915282611_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=04BXqyVT0bMAX9RJshx&tp=1&oh=d91e13721a3ace2311d5fbe4841228b2&oe=60576B66"
-                            alt="자바스크립트"
-                          />
-                        </div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="#">
-                        <div className="board_img">
-                          <img
-                            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/151213134_134563205204831_6028190961915282611_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=04BXqyVT0bMAX9RJshx&tp=1&oh=d91e13721a3ace2311d5fbe4841228b2&oe=60576B66"
-                            alt="자바스크립트"
-                          />
-                        </div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="#">
-                        <div className="board_img">
-                          <img
-                            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/151213134_134563205204831_6028190961915282611_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=04BXqyVT0bMAX9RJshx&tp=1&oh=d91e13721a3ace2311d5fbe4841228b2&oe=60576B66"
-                            alt="자바스크립트"
-                          />
-                        </div>
-                      </Link>
-                    </li>
+                    {this.state.createdMedias.map((media, idx) => {
+                      console.log(media);
+                      return (
+                        <li key={media._id}>
+                          <Link to={"/p/" + media._id}>
+                            <div className="board_img">
+                              <img
+                                src={media.media_url}
+                                alt={media.media_url}
+                              />
+                            </div>
+                          </Link>
+                        </li>
+                      );
+                    })}
                   </ul>
                 </div>
               </div>
