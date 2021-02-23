2a3f0b1b155249cbb1d9ca882384de0fc7cecde5
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-24 00:43:40 +0900

    React에서 Cloudinary에 이미지 업로드 Test

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 import "./css/PostDetail.css";
 
 class TestPage extends Component {
+  state = {
+    imageSelected: ""
+  };
+
+  uploadImage = () => {
+    console.log(this.state.imageSelected);
+    const formData = new FormData();
+    formData.append("file", this.state.imageSelected);
+    formData.append("upload_preset", "anstagram");
+
+    console.log(formData);
+
+    Axios.post(
+      "https://api.cloudinary.com/v1_1/anstagram123/image/upload",
+      formData
+    )
+      .then((response) => {
+        console.log(response.data.public_id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   render() {
-    return <h2>Test Page</h2>;
+    return (
+      <div style={{ marginTop: "80px" }}>
+        <input
+          type="file"
+          onChange={(event) => {
+            this.setState({ imageSelected: event.target.files[0] });
+          }}
+        />
+        <button onClick={this.uploadImage}>Upload Image</button>
+      </div>
+    );
   }
 }
 
