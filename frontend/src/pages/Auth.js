5bea9bf82e75e475e2b10a7e4a10c61a0ef219e4
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-25 13:57:05 +0900

    로그인/회원가입페이지 완료

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import "./css/Auth.css";
 import AuthContext from "../context/auth-context";
+
+import "./css/Reset.css";
+import "./css/Common.css";
+import "./css/Auth.css";
+
+const signupStyle = {
+  height: "600px"
+};
 
 class AuthPage extends Component {
   state = {
     isExist: false,
     isLogin: true,
-    isWrong: false
+    isWrong: false,
+    isActiveBtn: false
   };
 
   static contextType = AuthContext;
@@ -20,10 +29,59 @@ class AuthPage extends Component {
     this.fullnameEl = React.createRef();
   }
 
+  redColorInputOnChangeHandler = () => {
+    this.removeRedText();
+    this.isActiveBtnSetHandler();
+  };
+
+  isActiveBtnSetHandler = () => {
+    if (
+      this.state.isLogin &&
+      this.emailEl.current.value !== "" &&
+      this.passwordEl.current.value !== ""
+    ) {
+      this.setState({ isActiveBtn: true });
+      return;
+    }
+    if (
+      this.state.isLogin &&
+      this.emailEl.current.value === "" &&
+      this.passwordEl.current.value === ""
+    ) {
+      this.setState({ isActiveBtn: false });
+      return;
+    }
+    if (
+      !this.state.isLogin &&
+      this.emailEl.current.value !== "" &&
+      this.passwordEl.current.value !== "" &&
+      this.nicknameEl.current.value !== "" &&
+      this.fullnameEl.current.value !== ""
+    ) {
+      this.setState({ isActiveBtn: true });
+      return;
+    }
+    if (
+      !this.state.isLogin &&
+      this.emailEl.current.value === "" &&
+      this.passwordEl.current.value === "" &&
+      this.nicknameEl.current.value === "" &&
+      this.fullnameEl.current.value === ""
+    ) {
+      this.setState({ isActiveBtn: true });
+      return;
+    }
+  };
+
   switchModelHandler = () => {
+    console.log("switchModelHandler");
     this.setState((prevState) => {
       return { isLogin: !prevState.isLogin };
     });
+  };
+
+  removeRedText = () => {
+    this.setState({ isWrong: false, isExist: false });
   };
 
   submitHandler = (e) => {
@@ -94,6 +152,7 @@ class AuthPage extends Component {
         return res.json();
       })
       .then((resData) => {
+        this.setState({ isLogin: true });
         if (resData.data.createUser === null) {
           this.setState({ isExist: true });
           return;
@@ -114,41 +173,118 @@ class AuthPage extends Component {
 
   render() {
     return (
-      <form className="auth-form" onSubmit={this.submitHandler}>
-        <h2>{this.state.isLogin ? "Login" : "Signup"}</h2>
-        <div className="form-control">
-          <label htmlFor="email">이메일 주소</label>
-          <input type="email" id="email" ref={this.emailEl} />
-        </div>
-        {!this.state.isLogin && (
-          <>
-            <div className="form-control">
-              <label htmlFor="full_name">성명</label>
-              <input type="text" id="full_name" ref={this.fullnameEl} />
+      <div className="wrap">
+        <div className="container">
+          <div
+            className="login_inner"
+            style={this.state.isLogin ? null : signupStyle}
+          >
+            <header className="login_header">
+              <h1>
+                <span className="header_tit">로고</span>
+                <Link to="#" className="page_move">
+                  <img
+                    src="https://res.cloudinary.com/anstagram123/image/upload/v1613730289/anstagram/AnstagramLogo_uvski8.png"
+                    alt="main_logo"
+                  />
+                </Link>
+              </h1>
+            </header>
+
+            <form className="login_form" onSubmit={this.submitHandler}>
+              <div className="input_box">
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="이메일"
+                  ref={this.emailEl}
+                  onChange={this.redColorInputOnChangeHandler}
+                />
+              </div>
+              {!this.state.isLogin && (
+                <>
+                  <div className="input_box">
+                    <input
+                      type="text"
+                      id="full_name"
+                      ref={this.fullnameEl}
+                      placeholder="성명"
+                      onChange={this.isActiveBtnSetHandler}
+                    />
+                  </div>
+                  <div className="input_box">
+                    <input
+                      type="text"
+                      id="nickname"
+                      placeholder="닉네임"
+                      ref={this.nicknameEl}
+                      onChange={this.redColorInputOnChangeHandler}
+                    />
+                  </div>
+                </>
+              )}
+              <div className="input_box">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="비밀번호"
+                  ref={this.passwordEl}
+                  onChange={this.redColorInputOnChangeHandler}
+                />
+              </div>
+              {this.state.isWrong && this.state.isLogin && (
+                <div className="alt_text">
+                  <span>아이디 또는 비밀번호를 확인하세요</span>
+                </div>
+              )}
+              {this.state.isExist && !this.state.isLogin && (
+                <p className="alt_text">
+                  이미 존재하는 이메일 또는 닉네임 입니다.
+                </p>
+              )}
+              {this.state.isLogin && this.state.isActiveBtn && (
+                <div className="button_box">
+                  <button type="submit" className="active-btn">
+                    <span>로그인</span>
+                  </button>
+                </div>
+              )}
+              {this.state.isLogin && !this.state.isActiveBtn && (
+                <div className="button_box">
+                  <button type="submit" className="btn" disabled="disabled">
+                    <span>로그인</span>
+                  </button>
+                </div>
+              )}
+              {!this.state.isLogin && this.state.isActiveBtn && (
+                <div className="button_box">
+                  <button type="submit" className="active-btn">
+                    <span>회원가입</span>
+                  </button>
+                </div>
+              )}
+              {!this.state.isLogin && !this.state.isActiveBtn && (
+                <div className="button_box">
+                  <button type="submit" className="btn" disabled="disabled">
+                    <span>회원가입</span>
+                  </button>
+                </div>
+              )}
+            </form>
+            <div className="password_forget">
+              {this.state.isLogin ? (
+                <span className="hover-link" onClick={this.switchModelHandler}>
+                  회원가입
+                </span>
+              ) : (
+                <span className="hover-link" onClick={this.switchModelHandler}>
+                  로그인
+                </span>
+              )}
             </div>
-            <div className="form-control">
-              <label htmlFor="nickname">사용자 이름</label>
-              <input type="text" id="nickname" ref={this.nicknameEl} />
-            </div>
-          </>
-        )}
-        <div className="form-control">
-          <label htmlFor="password">비밀번호</label>
-          <input type="password" id="password" ref={this.passwordEl} />
+          </div>
         </div>
-        {this.state.isExist && (
-          <p className="redText">이미 존재하는 이메일 또는 닉네임 입니다.</p>
-        )}
-        {this.state.isWrong && (
-          <p className="redText">아이디 또는 비밀번호를 확인하세요!</p>
-        )}
-        <div className="form-actions">
-          <button type="submit">Submit</button>
-          <button type="button" onClick={this.switchModelHandler}>
-            Switch to {this.state.isLogin ? "Signup" : "Login"}
-          </button>
-        </div>
-      </form>
+      </div>
     );
   }
 }
