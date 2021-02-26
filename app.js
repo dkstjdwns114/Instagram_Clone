5c3678ddfb0bcabbbbebe90fcb94667a1de6de8e
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-26 13:19:52 +0900

    demonstration purposes

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
+const path = require("path");
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
+const isAuth = require("./middleware/is-auth");
+var cors = require("cors");
 
 const app = express();
 
-const graphQlSchema = require("./graphql/schema/index");
-const graphQlResolvers = require("./graphql/resolvers/index");
-
-const isAuth = require("./middleware/is-auth");
-
 app.use(bodyParser.json());
+
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8000",
+  "https://anstagram.herokuapp.com"
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed By CORS"));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -33,6 +53,13 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  });
+}
+
 mongoose.set("useFindAndModify", false);
 
 mongoose
@@ -41,7 +68,10 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
-    app.listen(8000);
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, (req, res) => {
+      console.log(`server listning on port: ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log(err);
